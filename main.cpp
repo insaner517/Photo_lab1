89c5a0c479eed924b8499ece31270496595fbc76
insaner <gzy517@sina.com>
2018-12-11 19:35:59 +0800

    add print detail

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ unsigned char *pBmpBuf;  //存储图像数据
 unsigned char *pColorBuf;  //存储图像数据
 BITMAPFILEHEADER fileheader;
 BITMAPINFOHEADER infoHead;
+void printDetial();
 bool readBmp(char *bmpName) {
 
     FILE *fp;
@@ -25,7 +26,7 @@ bool readBmp(char *bmpName) {
     fread(&infoHead,sizeof(BITMAPINFOHEADER),1,fp);   //从fp中读取BITMAPINFOHEADER信息到infoHead中,同时fp的指针移动
 
     if((sizeof(infoHead)+ sizeof(fileheader))!=fileheader.bfOffBits){
-        cout<<fileheader.bfOffBits<<" "<<infoHead.biSize<<"  "<< sizeof(fileheader)<<"  "<< sizeof(char);
+//        cout<<fileheader.bfOffBits<<" "<<infoHead.biSize<<"  "<< sizeof(fileheader)<<"  "<< sizeof(char);
         colornum = (fileheader.bfOffBits-(infoHead.biSize+ sizeof(fileheader)))/ sizeof(char);
         pColorBuf = new unsigned char[colornum];
         fread(pColorBuf,sizeof(char),colornum,fp);
@@ -33,14 +34,27 @@ bool readBmp(char *bmpName) {
     bmpwidth = infoHead.biWidth;
     bmpheight = infoHead.biHeight;
     linebyte = (bmpwidth*infoHead.biBitCount/8+3)/4*4; //计算每行的字节数，24：该图片是24位的bmp图，3：确保不丢失像素
-    cout<<infoHead.biBitCount;
+//    cout<<infoHead.biBitCount;
 //    linebyte = 512; //计算每行的字节数，24：该图片是24位的bmp图，3：确保不丢失像素
-    cout<<bmpwidth<<" "<<bmpheight<<"  "<< sizeof(fileheader)<<"  "<< sizeof(infoHead)<<"   "<<
-    fileheader.bfOffBits<<infoHead.biCompression<<endl;
+//    cout<<bmpwidth<<" "<<bmpheight<<"  "<< sizeof(fileheader)<<"  "<< sizeof(infoHead)<<"   "<<
+//    fileheader.bfOffBits<<infoHead.biCompression<<endl;
     pBmpBuf = new unsigned char[linebyte*bmpheight];
     fread(pBmpBuf,sizeof(char),linebyte*bmpheight,fp);
     fclose(fp);   //关闭文件
+    printDetial();
     return TRUE;
+}
+void printDetial(){
+    cout<<"bfType:"<<fileheader.bfType<<endl;
+    cout<<"bfSize:"<<fileheader.bfSize<<endl;
+    cout<<"bfOffBits:"<<fileheader.bfOffBits<<endl;
+
+    cout<<"bfSize:"<<fileheader.bfSize<<endl;
+    cout<<"biBitCount:"<<infoHead.biBitCount<<endl;
+    cout<<"biWidth:"<<infoHead.biWidth<<endl;
+    cout<<"biHeight:"<<infoHead.biHeight<<endl;
+    cout<<"biSize:"<<infoHead.biSize<<endl;
+    cout<<"biSizeImage:"<<infoHead.biSizeImage<<endl;
 }
 void writeBmp(){
     char writeFileName[] = "..\\lena_C_cut.bmp";
@@ -55,11 +69,11 @@ void writeBmp(){
     fwrite(&fileheader_cut,sizeof(fileheader_cut),1,fp);
     fwrite(&infoHead_cut,sizeof(infoHead_cut),1,fp);
     newBmpBuf = new unsigned char[linebyte*bmpheight/4];
-    cout<<bmpheight<<linebyte;
+//    cout<<bmpheight<<linebyte;
     if((sizeof(infoHead)+ sizeof(fileheader))!=fileheader.bfOffBits){
         fwrite(pColorBuf, sizeof(char),colornum,fp);
     }
-    cout<<linebyte<<"sxa"<<bmpheight<<endl;
+//    cout<<linebyte<<"sxa"<<bmpheight<<endl;
     for(int i=bmpheight/2;i<bmpheight;i++){
         for(int j=0;j<linebyte/2;j++){
 //            cout<<(int)pBmpBuf[i*linebyte+j];
@@ -67,7 +81,7 @@ void writeBmp(){
              newBmpBuf[(i-bmpheight/2)*linebyte/2+j] = pBmpBuf[i*linebyte+j];
         }
     }
-    cout<<(int)pBmpBuf[0];
+//    cout<<(int)pBmpBuf[0];
     if(!fwrite(newBmpBuf, sizeof(char),linebyte*bmpheight/4,fp)){
         cout<<"写入失败";
     }
